b647d99ffebfd2366388405c6b525274de1235b9
avasserman <alvasserman@gmail.com>
2023-02-14 13:47:31 -0500

    WIP. HW3

--- query_process.py
+++ query_process.py
@@ -2,33 +2,58 @@ from typing import List
 
 from documents import DocumentCollection, Document
 from index import Index
-from tokenizer import NaiveTokenizer
+from tokenizer import Tokenizer, NaiveTokenizer
 
 
-def process_query(query: str) -> List[str]:
-    return NaiveTokenizer().tokenize(query)
+class QueryTransformer:
+    def process_query(self, query: str) -> List[str]:
+        pass
 
 
-def format_single_result(doc: Document) -> str:
-    return doc.text
+class TokenizerOnlyQueryTransformer(QueryTransformer):
+    def __init__(self, tokenizer: Tokenizer):
+        self.tokenizer = tokenizer
+
+    def process_query(self, query: str) -> List[str]:
+        return self.tokenizer.tokenize(query)
 
 
-def format_results(results: List[str], documents: DocumentCollection) -> str:
-    """
-    Given the output of search use documents to create a string to be presented to the user.
-    :param results: List of doc_ids
-    :param documents: DocumentCollection to be used to format results.
-    :return: A single string presented to the user.
-    """
-    out = ''
-    for doc_id in results:
-        doc = documents.get(doc_id)
-        out += '\n' + format_single_result(doc)
-    return out
+class ResultFormatter:
+    def format_results(self, results: List[str]) -> str:
+        """
+        Given a list of doc_ids corresponding to the results of search create a string describing
+        these results to be presented to the user.
+        :param results: List of doc_ids
+        :return: A single string presented to the user.
+        """
+        pass
 
 
-def query_process(query: str, documents: DocumentCollection, index: Index) -> str:
-    processed_query = process_query(query)
-    results = index.search(processed_query)
-    formatted_results = format_results(results, documents)
-    return formatted_results
+class NaiveResultFormatter:
+    def __init__(self, document_collection: DocumentCollection):
+        self.document_collection = document_collection
+
+    @staticmethod
+    def format_single_result(doc: Document) -> str:
+        return doc.text
+
+    def format_results(self, results: List[str]) -> str:
+        out = ''
+        for doc_id in results:
+            doc = self.document_collection.get(doc_id)
+            out += '\n' + self.format_single_result(doc)
+        return out
+
+
+class QueryProcess:
+    def __init__(self, index: Index,
+                 query_transformer: QueryTransformer, result_formatter: ResultFormatter):
+        self.index = index
+        self.query_transformer = query_transformer
+        self.result_formatter = result_formatter
+
+    def run(self, query: str) -> str:
+        processed_query = self.query_transformer.process_query(query)
+        results = self.index.search(processed_query)
+        formatted_results = self.result_formatter.format_results(results)
+        return formatted_results
